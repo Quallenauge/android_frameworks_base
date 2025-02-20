c37a93d6bfbb1afe56e6e6220f619183545908e9
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-20 13:04:38 +0800

    Add CPU sleep optimizations
    Change-Id: Ic5ffb65f5ac199ef5f54d441a21229f6a73a6ef7
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -55,6 +55,7 @@ import android.hardware.devicestate.DeviceStateManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.metrics.LogMaker;
 import android.net.Uri;
+import android.os.BatteryManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -228,6 +229,7 @@ import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.util.DumpUtilsKt;
+import com.android.systemui.util.MediaSessionManagerHelper;
 import com.android.systemui.util.WallpaperController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.concurrency.MessageRouter;
@@ -269,7 +271,7 @@ import javax.inject.Provider;
  * {@link ActivityStarterImpl}
  */
 @SysUISingleton
-public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
+public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, MediaSessionManagerHelper.MediaMetadataListener {
 
     private static final int MSG_LAUNCH_TRANSITION_TIMEOUT = 1003;
     // 1020-1040 reserved for BaseStatusBar
@@ -377,6 +379,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     DozeServiceHost mDozeServiceHost;
     private final LightRevealScrim mLightRevealScrim;
     private PowerButtonReveal mPowerButtonReveal;
+    private final MediaSessionManagerHelper mMediaSessionManagerHelper;
 
     /**
      * Whether we should delay the wakeup animation (which shows the notifications and moves the
@@ -849,6 +852,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         if (PredictiveBackSysUiFlag.isEnabled()) {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
+        mMediaSessionManagerHelper = MediaSessionManagerHelper.Companion.getInstance(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1467,6 +1471,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(lineageos.content.Intent.ACTION_SCREEN_CAMERA_GESTURE);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
+        mMediaSessionManagerHelper.addMediaMetadataListener(this);
     }
 
     protected QS createDefaultQSFragment() {
@@ -2593,6 +2598,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            doCpuStandbyOptimization(true);
         }
 
         @Override
@@ -2659,6 +2665,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            doCpuStandbyOptimization(false);
         }
 
         /**
@@ -2719,6 +2726,34 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             updateScrimController();
         }
     };
+    
+    private void doCpuStandbyOptimization(boolean enable) {
+        BatteryManager batteryManager = (BatteryManager) mContext.getSystemService(Context.BATTERY_SERVICE);
+        boolean isChargingOrPlugged = batteryManager != null && 
+                (batteryManager.isCharging() || isPluggedIn());
+
+        boolean isMediaPlaying = mMediaSessionManagerHelper.isMediaPlaying();
+        boolean cpuStandbyOptEnabled =
+             SystemProperties.get("persist.sys.cpu_standby_optimization_enabled", "1").equals("1");
+
+        if (enable && (!cpuStandbyOptEnabled || isMediaPlaying || isChargingOrPlugged)) return;
+
+        SystemProperties.set("persist.sys.power_mode_limit_cpus", enable ? "1" : "0");
+    }
+
+    private boolean isPluggedIn() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = mContext.registerReceiver(null, ifilter);
+        if (batteryStatus == null) return false;
+
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+
+        return status == BatteryManager.BATTERY_STATUS_CHARGING ||
+               chargePlug == BatteryManager.BATTERY_PLUGGED_AC ||
+               chargePlug == BatteryManager.BATTERY_PLUGGED_USB ||
+               chargePlug == BatteryManager.BATTERY_PLUGGED_WIRELESS;
+    }
 
     /**
      * We need to disable touch events because these might
